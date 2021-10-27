682ea3317a2f959af75f79fa15eca173641ad37b
Spet <snippet0809@163.com>
2021-10-27 15:21:55 +0800

    feat:init

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/Users/spet/Documents/GitHub/snippet0809.github.io/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
